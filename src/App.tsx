5ad1c68db4cba7158e60d9f25d83e937afd7b24c
Alex Santamaria <alejandrosg11@gmail.com>
2024-08-13 17:04:53 -0600

    MatchGameComponent

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useAppSelector } from './hooks/store';
+import { MatchGame } from './components/MatchGame';
 
 function App() {
-  const [count, setCount] = useState(0)
   const stacks = useAppSelector((state) => state.stacks);
+  const countries = {Germany: "Berlin", France: "Paris", China: "Beijing"};
 
   console.log(stacks);
   return (
     <>
-      
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <MatchGame data={countries} />
+      </div>
     </>
   )
 }
---
+++ src/components/MatchGame.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+export const MatchGame = ( data: object ) => {
+    const [countryList] = useState(data)
+
+    console.log(countryList.data)
+
+    return(
+        <>
+
+        </>
+    )
+}
